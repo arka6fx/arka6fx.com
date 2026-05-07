efb88edc2fee35e14007ca5fdb137ce1ec01ba6a
arka <arkagarai292@gmail.com>
2026-05-08 00:43:30 +0530

    update pages to use new ProjectCard and BlogCard components

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,32 +1,23 @@
 import { Container } from "@/components/Container";
-import { Card } from "@/components/Card";
-import { CalendarDate } from "@/components/CalendarDate";
-
-const blogs = [
-  {
-    title: "day i switched to omarchy + lazyvim",
-    href: "https://smooth-barracuda-637.notion.site/Day-I-Switched-to-Omarchy-LazyVim-352561d525128099bf21e1fdfc18cd91",
-    date: "2026-03-06",
-  },
-];
+import { BlogCard } from "@/components/BlogCard";
+import { blogs } from "@/data/blogs";
 
 export default function BlogPage() {
   return (
     <Container>
       <section className="py-4 sm:py-6">
         <h1 className="text-2xl sm:text-3xl font-semibold mb-6">Blogs</h1>
-        <div className="space-y-4">
-          {blogs.map((blog) => (
-            <Card
+        <ul className="space-y-4">
+          {blogs.map((blog, index) => (
+            <BlogCard
               key={blog.title}
               title={blog.title}
               href={blog.href}
-              target="_blank"
-            >
-              <CalendarDate date={blog.date} />
-            </Card>
+              date={blog.date}
+              animationDelay={index * 100}
+            />
           ))}
-        </div>
+        </ul>
       </section>
     </Container>
   );
