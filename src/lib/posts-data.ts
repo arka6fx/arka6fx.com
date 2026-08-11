130ad0cfe9d16b865070137f5169743e653ce951
Arka Garai <arkagarai292@gmail.com>
2026-08-11 19:54:15 +0530

    chore: remove lazyvim blog post entirely

--- src/lib/posts-data.ts
+++ src/lib/posts-data.ts
@@ -3,16 +3,4 @@ export interface RawPost {
   content: string
 }
 
-const raw = `---
-title: "day i switched to omarchy + lazyvim"
-description: "My experience switching to Omarchy and LazyVim setup"
-date: "March 6, 2026"
-externalUrl: "https://app.notion.com/p/Day-I-Switched-to-Omarchy-LazyVim-352561d525128099bf21e1fdfc18cd91?source=copy_link"
----
-
-this post has been moved to notion. click the link to read it there.
-`
-
-export const postsData: RawPost[] = [
-  { slug: "lazyvim-switch", content: raw },
-]
+export const postsData: RawPost[] = []
